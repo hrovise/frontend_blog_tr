419b4125fff7cb6997aae4cdc52820a43b8e1ade
hrovi <hrovise@gmail.com>
2025-11-17 17:43:48 +0100

    update url link

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@
 
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:3000/api' // бекенд на Railway или другом хосте
+  apiUrl: 'https://backendblogtr-production.up.railway.app' // бекенд на Railway или другом хосте
 };
